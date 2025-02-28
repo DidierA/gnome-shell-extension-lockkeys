2a040833507b0cc8736c05bcc0452baf547ae752
DidierA <1620015+didiera@users.noreply.github.com>
2025-02-28 22:07:22 +0100

    Add file chooser for script

--- lockkeys@vaina.lt/prefs.js
+++ lockkeys@vaina.lt/prefs.js
@@ -20,7 +20,8 @@ const NOTIFICATIONS_ON = 'on';
 const NOTIFICATIONS_OSD = 'osd';
 
 export default class LockKeysPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
+    fillPreferencesWindow(window) { // window is an ExtensionPrefsDialog
+        //console.log(window) ;
         const group = new Adw.PreferencesGroup({
             title: _('Settings'),
             description: _('Change indicator display options')
@@ -29,6 +30,10 @@ export default class LockKeysPreferences extends ExtensionPreferences {
         const page = new Adw.PreferencesPage();
         page.add(group);
         window.add(page);
+
+        // window.connect('close-attempt', function(widget) {
+        //     console.log('closed');
+        // });
     }
 
     buildPrefsWidget() {
@@ -74,7 +79,54 @@ export default class LockKeysPreferences extends ExtensionPreferences {
         return this.createVerticalBoxCompat(indicator_style, notifications_style, enable_script, script_path);
     }
 
+    // Really, really ugly code here, to get access to the reference to the instance in a callback.
+    // This is nonsense. I sincerely hope that I suck at JS, Gtk, and so on, and a better solution exists.
+    fileEntryIconCallbackGen(this_ref,key) {
+        return function (entry, icon_pos) { return this_ref.fileEntryIconCallback(this_ref, key, entry, icon_pos); };
+    }
+
+    // called when the icon of the file entry is clicked.
+    fileEntryIconCallback(this_ref, key, entry, icon_pos) {
+        // only act on secondary icon press.
+        if (icon_pos == Gtk.EntryIconPosition.PRIMARY)
+             return ;
+
+        const file_dialog = new Gtk.FileDialog() ;
+        file_dialog.set_initial_file(Gio.File.new_for_path(entry.get_text()));
+        file_dialog.open(entry.get_ancestor(Gtk.Window), new Gio.Cancellable() , function(filedialog, result) {
+            let file ;
+            try {
+                file=filedialog.open_finish(result) ;
+            } catch (error) {
+                // silence 'Dismissed by User' exception.
+                if ( error instanceof Gtk.DialogError && error.message == "Dismissed by user") {
+                    return ;
+                }
+                throw(error) ;
+            }
+
+            // hopefully a file has been choosen
+            // check that it is executable
+            const file_info=file.query_info("*",Gio.FileQueryInfoFlags.NONE,null);
+            if (! file_info.get_attribute_boolean("access::can-execute")) {
+                const ine_dialog = Utils.createAlertDialog(
+                    entry,
+                    _('File is not executable!'),
+                    'File' + file.get_path() + ' is not executable',
+                );
+                ine_dialog.present(entry);
+                // the dialog has already been closed, what can we do?
+                return ;
+            }
+
+            // change entry to match
+            entry.set_text(file.get_path());
+            this_ref.getSettings().set_string(key, entry.get_text());
+        }) ;
+    }
+
     createEntryRow(key, text, tooltip) {
+
         let label = new Gtk.Label({ label: text, xalign: 0, tooltip_text:tooltip });
         const _settings = this.getSettings();
         const prev_value = _settings.get_string(key);
@@ -83,8 +135,13 @@ export default class LockKeysPreferences extends ExtensionPreferences {
         widget.set_text(prev_value);
         widget.halign = Gtk.Align.FILL;
 
+        // set secondary icon to launch file chooser
+        let icon = Gio.ThemedIcon.new_with_default_fallbacks('document-open-symbolic');
+        widget.set_icon_from_gicon(Gtk.EntryIconPosition.SECONDARY, icon) ;
+        widget.connect('icon-press', this.fileEntryIconCallbackGen(this,key)) ;
+        
         // called when enter key is pressed
-        // TODO: better handling 
+        // TODO: better handling, check if it has changed when window is closed
         widget.connect('activate', function(entry_widget) {
             const prev_value = _settings.get_string(key);
             
@@ -110,7 +167,7 @@ export default class LockKeysPreferences extends ExtensionPreferences {
             //    console.log(attr + ':' + file_info.get_attribute_as_string(attr)) ;
             //}
 
-            // Chekc if file is executable
+            // Check if file is executable
             if (! file_info.get_attribute_boolean("access::can-execute")) {
                 const ine_dialog = Utils.createAlertDialog(
                     entry_widget,
